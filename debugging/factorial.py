1777981df995b0c1b6a159dfd22975005455aab2
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-01-14 14:28:15 +1100

    Forgot to save changes before intital commit

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,8 +5,18 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= result
+        n -= 1  
     return result
 
-f = factorial(int(sys.argv[1]))
-print(f)
+if len(sys.argv) > 1:
+    try:
+        number = int(sys.argv[1])
+        if number < 0:
+            print("Factorial is not defined for negative numbers.")
+        else:
+            f = factorial(number)
+            print(f"The factorial of {number} is {f}")
+    except ValueError:
+        print("Please provide a valid integer.")
+else:
+    print("Usage: python script_name.py <number>")
